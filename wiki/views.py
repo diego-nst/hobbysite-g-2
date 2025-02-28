079e28333b556dd4e0e75be4e5171ed5fa0d0cad
Elly <hubert.olegario@student.ateneo.edu>
2025-03-01 00:55:21 +0800

    Created the Wiki List View and Wiki Detail View

--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Article
+
+
+class WikiListView(ListView):
+    model = Article
+    template_name = 'site_list.html'
+
+
+class WikiDetailView(DetailView):
+    model = Article
+    template_name = 'site_detail.html'
