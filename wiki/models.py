d08c48706fb2019d9aa0f474d47a34d20ca6806e
Elly <hubert.olegario@student.ateneo.edu>
2025-03-01 00:31:50 +0800

    Created the ArticleCategory and Article models

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('article_category', args=[str(self.name)])
+    
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory,
+                                   on_delete=models.SET_NULL)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', args=[str(self.pk)])
