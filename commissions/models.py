620fbdac27e724248453596c63515d7e7a7205ae
diego-nst <diegostanalgo1@gmail.com>
2025-03-02 16:47:14 +0800

    created the django models for commission and added commission objects to the context of the views

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    peopleRequired = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    commission = models.ForeignKey(Commission, on_delete=models.SET_NULL, null=True, related_name='comment')
+    entry = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- commissions/views.py
+++ commissions/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from .models import Commission
+
 
 class CommissionListView(ListView):
+    model = Commission
     template_name = 'commissions/commission_list.html'
 
 
 class CommissionDetailView(DetailView):
+    model = Commission
     template_name = 'commissions/commission_detail.html'
 
 
 def commission_list(request):
-    ctx = {}
+    ctx = {"commissions": Commission.objects.all()}
     return render(request, 'commissions/commission_list.html',ctx)
 
 
 def commission_detail(request, pk):
-    ctx = {}
+    ctx = {"commssion": Commission.objects.get(pk=pk)}
     return render(request, 'commissions/commission_detail.html', ctx)
-# Create your views here.
