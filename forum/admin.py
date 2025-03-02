8b4a378314fe4a7b948ccca3f536bb7ca393acb1
bea-ndc <beatrice.decastro@student.ateneo.edu>
2025-03-02 15:29:44 +0800

    Updated the admin.py file
    Created the admin panel for the PostCategory model and an inline admin panel for the Post model

--- forum/admin.py
+++ forum/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Post, PostCategory
 
 
+class PostInline(admin.TabularInline):
+    model = Post
 
 
+class PostCategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline,]
 
+
+admin.site.register(PostCategory, PostCategoryAdmin)
 
 # Register your models here.
