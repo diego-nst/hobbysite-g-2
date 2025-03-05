8a40aaaa586f59e6e821a90286f953644f2d2651
ellyyy-son <167102704+ellyyy-son@users.noreply.github.com>
2025-03-05 13:47:17 +0800

    Update urls.py
    Fixed url typo

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,7 +3,7 @@ from .views import WikiListView, WikiDetailView
 
 
 urlpatterns = [
-    path('wiki/article', WikiListView.as_view(), name='article_list'),
+    path('wiki/articles', WikiListView.as_view(), name='article_list'),
     path('wiki/article/<int:pk>', WikiDetailView.as_view(), name='article_detail')
 ]
 
