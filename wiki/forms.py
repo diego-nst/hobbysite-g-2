217ab49ed74658c9e56b668b700c457944b57114
Elly <hubert.olegario@student.ateneo.edu>
2025-04-15 23:21:41 +0800

    Created the forms.py for the Articles

---
+++ wiki/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Article, ArticleCategory
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'entry', 'category', 'entry', 'header_image']
+        widgets = {
+            'category': forms.ChoiceField(choices=ArticleCategory.objects.all)
+        }
+
