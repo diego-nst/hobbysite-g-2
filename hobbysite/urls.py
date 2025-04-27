1d0d8c653bab99271bacbfa9a99c55f294d2dbaf
diego-nst <diegostanalgo1@gmail.com>
2025-04-27 13:12:51 +0800

    Added the user_management app and a working login flow

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('commissions.urls', namespace='commissions')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
