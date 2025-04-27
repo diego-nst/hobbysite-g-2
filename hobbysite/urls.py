5aa1b2bae6109a6b6a066076df9468ec574f7fea
diego-nst <diegostanalgo1@gmail.com>
2025-04-27 13:15:43 +0800

    changed url to make "commissions/" come from the project wide urls instead of the app urls

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('commissions.urls', namespace='commissions')),
+    path('commissions/', include('commissions.urls', namespace='commissions')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
