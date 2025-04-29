a0cb681433af099a9bfb92b66dfcf2f53028b974
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2025-04-30 00:30:17 +0800

    added profile model and inline for user_management

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=63)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.display_name
+    
