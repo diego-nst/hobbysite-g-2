82ffa0e78a9b3a01b0c1083636ce6532782e7ecf
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2025-04-30 21:14:15 +0800

    added models, added forms, created update and create html files

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Article, ArticleCategory
+from .forms import WikiForm, CommentForm
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
+
 
 from .models import Article
 
@@ -7,12 +15,14 @@ from .models import Article
 
 
 class BlogListView(ListView):
-
     model = Article
     template_name = 'blog_list.html'
 
 
 class BlogDetailView(DetailView):
-
     model = Article
     template_name = 'blog_detail.html'
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Article
