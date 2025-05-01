6af12c1cb2ff344297085d7a3e66a86e28062209
diego-nst <diegostanalgo1@gmail.com>
2025-05-01 10:59:28 +0800

    working password and login flow

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,7 @@ urlpatterns = [
     path('', include('commissions.urls', namespace='commissions')),
     path('', include('merchstore.urls', namespace='merchstore')),
     path('', include('forum.urls', namespace='forum')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
