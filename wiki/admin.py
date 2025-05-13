2ba7ec45e249250b54cd5ae88752a2c64310968c
Elly <hubert.olegario@student.ateneo.edu>
2025-05-13 23:26:37 +0800

    Added comments and edited for PEP8 Compliance

--- wiki/admin.py
+++ wiki/admin.py
@@ -3,24 +3,37 @@ from .models import ArticleCategory, Article, Comment, ArticleImage
 
 
 class ArticleInline(admin.TabularInline):
+    '''
+    Admin in line for Article
+    '''
     model = Article
 
 
 class CommentInLine(admin.TabularInline):
+    '''
+    Admin in line for Comment
+    '''
     model = Comment
 
 
 class ArticleImageInline(admin.TabularInline):
+    '''
+    Admin in line for Article Image
+    '''
     model = ArticleImage
 
 
 class ArticleAdmin(admin.ModelAdmin):
+    '''
+    Admin panel for Article with Comments and ArticleImage as in lines
+    '''
     inlines = [CommentInLine,  ArticleImageInline]
 
 
-
-
 class ArticleCategoryAdmin(admin.ModelAdmin):
+    '''
+    Admin panel for ArticleCategory with Article as in line
+    '''
     inlines = [ArticleInline]
 
 
--- wiki/forms.py
+++ wiki/forms.py
@@ -3,6 +3,9 @@ from .models import Article, ArticleCategory, Comment, ArticleImage
 
 
 class WikiForm(forms.ModelForm):
+    '''
+    This is the form used in the Create View and Update View either to create or update articles.
+    '''
     class Meta:
         model = Article
         fields = ['title', 'entry', 'category', 'header_image']
@@ -12,12 +15,18 @@ class WikiForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
+    '''
+    This is the form used in creating the comments
+    '''
     class Meta:
         model = Comment
         fields = ['entry']
 
 
 class ArticleImageForm(forms.ModelForm):
+    '''
+    This is the form used to add images into the Image Gallery
+    '''
     class Meta:
         model = ArticleImage
         fields = ['image', 'description']
