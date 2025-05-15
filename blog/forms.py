2634712e40831b50927de5eef9fcf90803c1346c
QEBSOWO <168461585+QEBSOWO@users.noreply.github.com>
2025-05-15 23:37:30 +0800

    added comments to blog/forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,11 @@ from .models import Article, ArticleCategory, Comment, ArticleImage
 
 
 class BlogForm(forms.ModelForm):
+    '''
+    Form for the Blog Article Model
+
+    Only contains title, entry, category, and header image 
+    '''
     class Meta:
         model = Article
         fields = ['title', 'entry', 'category', 'header_image']
@@ -12,12 +17,22 @@ class BlogForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
+    '''
+    Form for the Comment Form
+
+    Contains comment entry
+    '''
     class Meta:
         model = Comment
         fields = ['entry']
 
 
 class ArticleImageForm(forms.ModelForm):
+    '''
+    Form for ArticleImage, used for the gallery
+
+    Contains ArticleImage and description
+    '''
     class Meta:
         model = ArticleImage
         fields = ['image', 'description']
