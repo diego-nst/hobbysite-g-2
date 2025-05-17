5d178dc37350c3e6e73e6e41abbf52d7293d5ff1
moira-b <moiraysabell7@gmail.com>
2025-05-17 13:19:44 +0800

    Added docstring comments for merchstore admin.py

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -3,15 +3,25 @@ from .models import Product, ProductType, Transaction
 
 
 class ProductInLine(admin.TabularInline):
+    '''
+    InLine for the Product model under the ProductType model.
+    '''
     model = Product
 
 
 class ProductTypeAdmin(admin.ModelAdmin):
+    '''
+    Admin for the ProductType model. 
+    It has one inline, which is the Product model.
+    '''
     model = ProductType
     inlines = [ProductInLine]
 
 
 class TransactionAdmin(admin.ModelAdmin):
+    '''
+    Admin for the Transaction model.
+    '''
     model = Transaction
 
 
